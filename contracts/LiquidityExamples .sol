14b80f19f374036e76c89992e73171cf373ed698
epomatti <evandro.pomatti@gmail.com>
2022-02-19 11:46:26 -0300

    increase liquidity

--- contracts/LiquidityExamples .sol
+++ contracts/LiquidityExamples .sol
@@ -232,4 +232,36 @@ contract LiquidityExamples is IERC721Receiver, LiquidityManagement {
         //send liquidity back to owner
         _sendToOwner(tokenId, amount0, amount1);
     }
+
+    /// @notice Increases liquidity in the current range
+    /// @dev Pool must be initialized already to add liquidity
+    /// @param tokenId The id of the erc721 token
+    /// @param amount0 The amount to add of token0
+    /// @param amount1 The amount to add of token1
+    function increaseLiquidityCurrentRange(
+        uint256 tokenId,
+        uint256 amountAdd0,
+        uint256 amountAdd1
+    )
+        external
+        returns (
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        )
+    {
+        INonfungiblePositionManager.IncreaseLiquidityParams
+            memory params = INonfungiblePositionManager
+                .IncreaseLiquidityParams({
+                    tokenId: tokenId,
+                    amount0Desired: amountAdd0,
+                    amount1Desired: amountAdd1,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                });
+
+        (liquidity, amount0, amount1) = nonfungiblePositionManager
+            .increaseLiquidity(params);
+    }
 }
